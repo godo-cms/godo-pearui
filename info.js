cc8ad47229008be11a8eb3d755ddfc62eac2d005
icanwallet <96780182+godocms@users.noreply.github.com>
2021-12-30 11:26:00 +0800

    init

---
+++ info.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "key": "godo_pearui",
+    "dir": __dirname,
+    "info": {
+        "author": "godocms",
+        "url": "https://godocms.com",
+        "email" : "4294979@qq.com"
+    },
+    "url": "ui",
+    "type": "ui",
+    "name": "ui"
+}
